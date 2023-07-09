b3595c6b03e593ecfe2dddadc755e8f6003cee9b
Mary Williams <sbyardnss@gmail.com>
2023-07-09 15:05:15 -0500

    updated

--- carsello/settings.py
+++ carsello/settings.py
@@ -49,10 +49,10 @@ INSTALLED_APPS = [
 # THIS IS NEW
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated',
     ],
 }
 
