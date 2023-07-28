946bdc8f7e4a47f85398a77bacdc522919c5365f
Mary Williams <sbyardnss@gmail.com>
2023-07-28 11:23:25 -0500

    added notes to order model for capture of order information

--- carsello_api/models/order.py
+++ carsello_api/models/order.py
@@ -9,3 +9,22 @@ class Order(models.Model):
     item = models.ForeignKey('Artwork', on_delete=models.DO_NOTHING)
     paypal_order_id = models.CharField(max_length=75)
     
+# ordered_item (fk)
+
+# paypal_id charfield
+# create_time charfield
+# customer_id charfield
+# customer_email charfield
+# customer_name charfield
+# order_amount positiveInteger
+# payment_id charfield?
+# payment_status charfield
+# payment_protection charfield
+# payment_status charfield
+# reference_id charfield
+# shipping_street_address charfield
+# shipping_city char
+# shipping_state char
+# shipping_country char
+# shipping_zipcode char
+# order_status char
